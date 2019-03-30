2f9492aaa50063ba03aa016fb1777106d630372e
Souporno Ghosh <souporno2000@gmail.com>
2019-03-30 20:13:17 +0530

    Initial commit

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour {
+
+	public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+	// Update is called once per frame
+	void Update () 
+	{
+        if (doSpin)
+        {
+            //Rotate the game object about up (x) axis
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+
+        if (doMotion)
+        {
+            //Move the game object up and down
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }
+	}
+}
---
+++ Assets/Scripts/BombBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombBehavior : MonoBehaviour {
+
+    //the explosion prefab goes here
+    public GameObject explosionPrefab;
+    //the explosion prefab for target goes here
+    public GameObject targetExplosionPrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Projectile")
+        {
+            //look for each game ogject with tag "Target"
+            foreach (GameObject target in GameObject.FindGameObjectsWithTag("Target"))
+            {
+                //destroy the target
+                Destroy(target);
+
+                //show the explosion
+                Instantiate(targetExplosionPrefab, target.transform.position, target.transform.rotation);
+            }
+
+            if (explosionPrefab)
+            {
+                // Instantiate an explosion effect at the gameObjects position and rotation
+                Instantiate(explosionPrefab, transform.position, transform.rotation);
+            }
+
+            // destroy the projectile
+            Destroy(collision.gameObject);
+
+            // destroy self
+            Destroy(gameObject);
+        }
+    }
+}
